8745c6b397280f5621cd97bc983a603f8d6fff2d
Kan <kantippayamontri@gmail..com>
2024-10-04 21:57:43 +0700

    use tree in data structure

--- graph_constants.py
+++ graph_constants.py
@@ -1,30 +1,130 @@
 from pydantic import BaseModel, Field
 
+
 class Device(BaseModel):
-    id:str
+    id: str
     name: str = Field(alias="device_name")
     type: str = Field(alias="device_type")
     unprocessed_data: list[str] = Field(alias="device_unprocessed")
     raw_data: dict[str, dict[str, str]] | None = Field(alias="raw_data")
 
-class Service(BaseModel):
-    ...
-    
+
+device_category_mapping = {
+    ("Average", "Daily", "Low"): "Private",
+    ("Transfer", "Daily", "Low"): "Private",
+    ("Anonymise", "Daily", "Low"): "Private",
+    ("Average", "Daily", "High"): "Protected",
+    ("Transfer", "Daily", "High"): "Protected",
+    ("Anonymise", "Daily", "High"): "Protected",
+    ("Average", "Daily", "Medium"): "Secert",
+    ("Transfer", "Daily", "Medium"): "Secert",
+    ("Anonymise", "Daily", "Medium"): "Secert",
+    ("Average", "Weekly", "Low"): "Confidential",
+    ("Transfer", "Weekly", "Low"): "Confidential",
+    ("Anonymise", "Weekly", "Low"): "Confidential",
+    ("Average", "Weekly", "High"): "Restricted",
+    ("Transfer", "Weekly", "High"): "Restricted",
+    ("Anonymise", "Weekly", "High"): "Restricted",
+    ("Average", "Monthly", "Low"): "Private",
+    ("Transfer", "Monthly", "Low"): "Private",
+    ("Anonymise", "Monthly", "Low"): "Private",
+    ("Average", "Monthly", "High"): "Protected",
+    ("Transfer", "Monthly", "High"): "Protected",
+    ("Anonymise", "Monthly", "High"): "Protected",
+    ("Average", "Monthly", "Medium"): "Protected",
+    ("Transfer", "Monthly", "Medium"): "Protected",
+    ("Anonymise", "Monthly", "Medium"): "Protected",
+    ("Average", "Yearly", "Low"): "Confidential",
+    ("Transfer", "Yearly", "Low"): "Confidential",
+    ("Anonymise", "Yearly", "Low"): "Confidential",
+    ("Average", "Yearly", "High"): "Restricted",
+    ("Transfer", "Yearly", "High"): "Secret",
+    ("Average", "Yearly", "High"): "Restricted",
+    ("Upload", "Yearly", "Medium"): "Protected",
+}
+
+
+class Service(BaseModel): ...
+
+
 class Data(BaseModel):
-    id:str
+    id: str
     label: str
 
+
 class Position(BaseModel):
-    x:int 
-    y:int
+    x: int
+    y: int
 
-class RelationData(BaseModel):
-    source: str
-    target: str
 
-class Node(BaseModel):
+# class RelationData(BaseModel):
+#     source: str
+#     target: str
+
+
+class GraphNode(BaseModel):
     data: Data
     position: Position
 
-class Relation(BaseModel):
-    data: RelationData | None
+
+# class Relation(BaseModel):
+#     data: RelationData | None
+
+
+class Node:
+    def __init__(self, id: str, label: str):
+        self.id: str = id
+        self.label: str = label
+        self.children: list[Node] | None = []
+        self.parent: list[Node] = []
+
+
+class GraphTree:
+    def __init__(self, root: Node):
+        self.root = root
+
+    def get_root(self):
+        return self.root
+
+    def add_child(self, parent_node: Node | None, child_node: Node):
+        if parent_node is not None:
+            child_node.parent.append(parent_node)
+            child_node = self.append_id_parent(
+                node=child_node
+            )  # change id by adding parent id
+            for _parent_node in child_node.parent:
+                _parent_node.children.append(child_node)
+        return child_node
+
+    def append_id_parent(self, node: Node):
+        if node.parent is not None:
+            parent_id = ""
+
+            for parent in node.parent:
+                parent_id += parent.id
+
+            node.id = f"{parent_id}_{node.id}".replace(" ", "_")
+        return node
+
+    def print_tree(self, show_id=False, show_level=False):
+        self._print_tree_recursive(
+            node=self.root, show_id=show_id, show_level=show_level
+        )
+
+    def _print_tree_recursive(self, node, level=0, show_id=False, show_level=False):
+        show_string = str(node.label)
+
+        if show_level:
+            show_string = f"[{level}] " + show_string
+
+        show_string = "  " * level + show_string
+
+        if show_id:
+            show_string = show_string + f" ({str(node.id)})"
+
+        print(show_string)
+
+        for child in node.children:
+            self._print_tree_recursive(
+                node=child, level=level + 1, show_id=show_id, show_level=show_level
+            )
--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-
 from icecream import ic
-from graph_constants import (
-    Device,
-    Service,
-    Data,
-    Position,
-    RelationData,
-    Node,
-    Relation,
-)
+from graph_constants import *
 from mock_data import data
-import random
 
 
 # code to generate graph
@@ -41,63 +31,126 @@ def create_elements(
     return devices, services
 
 
-def create_device_graph(
-    device: Device, start_h:int,slot_h:int, hslot_h:int
-) -> list[Node | Relation] | None:
-    ic(device.model_dump_json())
-    device_node_relation_list: list[Node | Relation] = []
-    # Create node device node
-    device_node = Node(
-        data=Data(id="d_" + device.id, label=device.name), position=Position(x=0, y=start_h)
+# def create_device_graph(
+#     device: Device, start_h:int,slot_h:int, hslot_h:int
+# ) -> list[Node | Relation] | None:
+#     ic(device.model_dump_json())
+#     device_node_relation_list: list[Node | Relation] = []
+#     # Create node device node
+#     device_node = Node(
+#         data=Data(id="d_" + device.id, label=device.name), position=Position(x=0, y=start_h)
+#     )
+#     device_node_relation_list.append(device_node)
+
+#     # Create unprocessed node
+#     for un_index ,_un in enumerate(device.unprocessed_data):
+#         un_node = Node(
+#             data=Data(id=f"un_{device.id}_{un_index}", label=_un),
+#             position=Position(x=50, y=start_h)
+#         )
+
+#         device_node_relation_list.append(un_node)
+
+#         # Create relation device_node and unprocessed
+#         relation_node = Relation(
+#             data=RelationData(
+#                 source=device_node.data.id,
+#                 target=un_node.data.id
+#             )
+#         )
+
+#         device_node_relation_list.append(relation_node)
+
+#         if device.raw_data is not None: # found raw data
+#             if _un in device.raw_data.keys(): # unprocessed data has details
+#                 #create action node
+#                 action_node = Node(
+#                     data=Data(
+#                        id=f"action_{device.id}_{un_index}",
+#                        label=device.raw_data[_un]["action"]
+#                         ),
+#                     position=Position(
+#                         x=100,
+#                         y=start_h,
+#                     )
+#                 )
+
+#                 device_node_relation_list.append(action_node)
+
+#         start_h += hslot_h
+
+
+#     return device_node_relation_list
+
+
+def create_device_tree(device: Device | None):
+    # ic(device.model_dump())
+    if device is None:
+        return None
+
+    # Create Tree and add device node
+    tree = GraphTree(
+        root=Node(id=str("d_" + device.id).replace(" ", "_"), label=device.name)
     )
-    device_node_relation_list.append(device_node)
+    root = tree.get_root()
+    # Create Device type Node
+    if device.type is not None:
+        device_type_node = Node(id="dt_" + device.type, label=device.type)
+        current_node = tree.add_child(parent_node=root, child_node=device_type_node)
 
-    # Create unprocessed node
-    for un_index ,_un in enumerate(device.unprocessed_data):
-        un_node = Node(
-            data=Data(id=f"un_{device.id}_{un_index}", label=_un),
-            position=Position(x=50, y=start_h)
-        )
+    device_type_node_temp = current_node  # for separate unprocessed data branch
 
-        device_node_relation_list.append(un_node)
-
-        # Create relation device_node and unprocessed
-        relation_node = Relation(
-            data=RelationData(
-                source=device_node.data.id,
-                target=un_node.data.id
+    # Create Device unprocessed Node
+    if device.unprocessed_data is not None:
+        for _un in device.unprocessed_data:
+            un_processed_node = Node(id="un_" + _un, label=_un)
+            current_node = tree.add_child(
+                parent_node=device_type_node_temp, child_node=un_processed_node
             )
-        )
 
-        device_node_relation_list.append(relation_node)
+            # Create raw data
+            if device.raw_data is None:
+                continue
 
-        if device.raw_data is not None: # found raw data
-            if _un in device.raw_data.keys(): # unprocessed data has details
-                #create action node 
-                action_node = Node(
-                    data=Data(
-                       id=f"action_{device.id}_{un_index}",
-                       label=device.raw_data[_un]["action"]
-                        ),
-                    position=Position(
-                        x=100,
-                        y=start_h,
+            if _un in device.raw_data.keys():  # found raw data
+
+                # Create category -> sensitivity node
+                sensitivity = device_category_mapping[
+                    (
+                        device.raw_data[_un]["action"],
+                        device.raw_data[_un]["frequency"],
+                        device.raw_data[_un]["sensitivity"],
                     )
+                ]
+                sensitivity_node = Node(id="sen_" + sensitivity, label=sensitivity)
+                current_node = tree.add_child(parent_node=current_node, child_node=sensitivity_node)
+
+                # Create action node
+                action = device.raw_data[_un]["action"]
+                action_node = Node(id="at_" + action, label=action)
+                current_node = tree.add_child(
+                    parent_node=current_node, child_node=action_node
                 )
 
-                device_node_relation_list.append(action_node)
-            
-        start_h += hslot_h 
+                # Create action unprocessed node #TODO: orange node
+                action_unprocessed = action + "_" + _un
+                action_unprocessed_node = Node(id="atun_" + action_unprocessed, label=action_unprocessed.replace("_", "/n"))
 
 
+    # print(tree._print_tree_recursive(node=current_node_temp))
 
+    # if current_node is not None:
+    #     if device.unprocessed_data:
+    #         for _un in device.unprocessed_data:
+    #             unprocess_node =
 
-    return device_node_relation_list
+    return tree
 
 
 # TODO: making graph
 from dash import Dash, html
 import dash_cytoscape as cyto
+
 # import tkinter as tk
 
 app = Dash(__name__)
@@ -108,6 +161,9 @@ app = Dash(__name__)
 
 screen_width = 2000
 screen_heigth = 2000
+
+device_screen_width = service_screen_width = int(screen_width / 2)
+device_screen_heigth = service_screen_heigth = int(screen_heigth / 2)
 
 devices, services = create_elements(data)
 if devices:
@@ -123,42 +179,46 @@ if services:
 # prepare node for device
 device_graph_list = []
 if devices is not None:
-    
-    # count number of mini-slot of each device 
+
+    # count number of mini-slot of each device
     """
     device with unprocessed = #unprocessed * 2
     device with unprocessed and raw_data = #unprocessed * (4.5)
     space = (2 + (#device -1)) / 2
     """
     number_slot = 0
-    number_slot += (2 + len(devices)-1) / 2.0 # for space
+    number_slot += (2 + len(devices) - 1) / 2.0  # for space
     for device in devices:
-        #check device has unprocessed data
+        # check device has unprocessed data
         if len(device.unprocessed_data) > 0:
-            #check has raw_data or not
-            if device.raw_data is None: #found only unprocessed
-                number_slot += len(device.unprocessed_data) * 2 
+            # check has raw_data or not
+            if device.raw_data is None:  # found only unprocessed
+                number_slot += len(device.unprocessed_data) * 2
             else:
                 number_slot += len(device.unprocessed_data) * 4.5
-    
-    slot = screen_heigth // int(number_slot + 0.5) #ceil the slot number
+
+    slot = screen_heigth // int(number_slot + 0.5)  # ceil the slot number
     hslot = int(slot / 2)
-    
+
     # ic(f"number slot: {number_slot}")
     # ic(f"slot: {slot}")
     # ic(f"hslot: {hslot}")
-    
+
     h_now = hslot
     for index, device in enumerate(devices):
+        # ic(device.model_dump())
         # devide screen height for each node
-        device_node = create_device_graph(device, start_h=h_now, slot_h=slot, hslot_h=slot)
-        h_now += 200 # for debug
+        # device_node = create_device_graph(device, start_h=h_now, slot_h=slot, hslot_h=slot)
+        # TODO: create device tree
+        device_tree = create_device_tree(device=device)
+        device_tree.print_tree(show_id=True, show_level=True)
+        # h_now += 200 # for debug
 
-        if device_node is not None:
-            for ele in device_node:
-                ic(ele.model_dump_json())
-                device_graph_list.append(ele.model_dump())
-            # exit()  # FIXME: remove this
+        # if device_node is not None:
+        #     for ele in device_node:
+        #         ic(ele.model_dump_json())
+        #         device_graph_list.append(ele.model_dump())
+        #     # exit()  # FIXME: remove this
 
 ic(screen_width, screen_heigth)
 
